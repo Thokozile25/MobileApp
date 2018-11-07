ceb2c4d9d4ebc1dd277070d9f5fbaaf0cf32d6e6
Thokozile25 <thokozilendlebe@gmail.com>
2018-11-07 14:37:23 +0200

    changes

--- vue1/practical/script.js
+++ vue1/practical/script.js
@@ -1,1 +1,25 @@
-console.log("running script")
+var app = new Vue({
+    el: "#app",
+    data: {
+        guess: 0,
+        userguess: 0,
+    },
+    methods: {
+        reverseMessage: function () {
+            if(this.$data.userguess == this.$data.guess){
+                alert ("Correct!")
+            }else {
+                alert ("Incorrect,try again.")
+            }
+                
+            
+        }
+    }
+})
+
+app.guess = Math.round(Math.random() * 10)
+
+console.log(app.guess)
+
+
+    
