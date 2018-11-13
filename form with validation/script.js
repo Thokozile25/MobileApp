d0bb13a73d5bcf45714aecef37b85f496a513d06
Thokozile25 <thokozilendlebe@gmail.com>
2018-11-13 14:21:03 +0200

    update

--- form with validation/script.js
+++ form with validation/script.js
@@ -5,22 +5,31 @@ var app = new Vue ({
         password:"",
         usernameError: false,
         passwordError: false,
-        usernameErrorMessage: []
+        usernameErrorMessage: [],
+        passwordErrorMessage: []
     },
     methods:{
         login: function(){
             this.usernameError = false
             this.passwordError = false 
             this.usernameErrorMessage = []
+            this.passwordErrorMessage = []
 
 
             if(this.password.length < 6) {
-                this.usernameError = true
+                this.passwordError = true
+                this.passwordErrorMessage.push ({msg:'Password is too short',date: Date.now()})
+                var errorMessage = {
+                    msg: 'new Error Message',
+                    date: Date.now()
+
             }
+            this.passwordErrorMessage.push(errorMessage)
+        }
 
             if(this.username.length < 3) {
                 this.usernameError = true
-                this.usernameErrorMessage.push({msg:'Username is too short',date: Date.now()})
+                this.usernameErrorMessage.push ({msg:'Username is too short',date: Date.now()})
                 var errorMessage = {
                     msg: 'new Error Message',
                     date: Date.now()
@@ -40,22 +49,21 @@ var app = new Vue ({
     }
 })
 // app.js
-
 window.addEventListener('load', function() {
 
-    var webAuth = new auth0.WebAuth({
-      domain: 'thokozilendlebe.auth0.com',
-      clientID: 'zcQl1KAq-txqnQu0PkDYl09uQXCMhCfa',
-      responseType: 'token id_token',
-      scope: 'openid',
-      redirectUri: window.location.href
-    });
-  
-    var loginBtn = document.getElementById('login');
-  
-    loginBtn.addEventListener('click', function(e) {
-      e.preventDefault();
-      webAuth.authorize();
-    });
-  
-  });
+   var webAuth = new auth0.WebAuth({
+     domain: 'thokozilendlebe.auth0.com',
+     clientID: 'zcQl1KAq-txqnQu0PkDYl09uQXCMhCfa',
+     responseType: 'token id_token',
+     scope: 'openid',
+     redirectUri: window.location.href
+   });
+ 
+   var loginBtn = document.getElementById('login');
+ 
+   loginBtn.addEventListener('click', function(e) {
+     e.preventDefault();
+     webAuth.authorize();
+   });
+ 
+ });
